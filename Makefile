b9876a0b290818b377b37ddb83f8bbe37f3c00a3
Scott Peterson <spete020@sledge.cs.ucr.edu>
2021-07-16 15:27:24 -0700

    added lab1.c lab1test.c

--- Makefile
+++ Makefile
@@ -182,7 +182,6 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
-	_lab1\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
--- lab1.c
+++ lab1.c
@@ -8,9 +8,52 @@
 #include "traps.h"
 #include "memlayout.h"
 
-int main() {
+void exitTest() {
 
-    
+    printf(1, "Testing exit\n");
+
+    int pid = fork(); 
+    int exitvar;
+    int childpid;
+
+    if (pid == 0) {
+        printf(1, "This is the child with pid %d\n", pid);
+        printf(1, "This child exits with status 1");
+        exit(1);
+    }
+    else {
+        childpid = wait(&exitvar);
+        printf(1, "This is the parent\n");
+        printf(1, "its child with pid: %d exits with status %d\n", childpid, exitvar);
+    }
+
+    exit(0);
+    printf(1, "This will not execute!\n");
+
+}
+
+void waitTest() {
+
+}
+
+void waitpidTest() {
+
+}
+
+int main(int argc, char* argv[]) {
+
+    if (atoi(argv[1]) == 1) {
+        exitTest();
+    }
+
+    else if (atoi(argv[2]) == 2) {
+        waitTest();
+    }
+
+    else if (atoi(argv[3]) == 3) {
+        waitpidTest();
+    }
+
 
     return 0;
 }
