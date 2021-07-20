4aac517b5300ed45b6bab5eb1d19c9ff8b67206e
Scott Peterson <spete020@sledge.cs.ucr.edu>
2021-07-20 13:59:52 -0700

    updated waitpid

--- lab1.c
+++ lab1.c
@@ -23,7 +23,7 @@ void exitTest() {
     }
     else {
         childpid = wait(&exitvar);
-        printf(1, "This is the parent, ");
+        printf(1, "This is the parent, with pid %d, ", pid);
         printf(1, "its child with pid: %d exits with status %d\n", childpid, exitvar);
     }
 
@@ -66,14 +66,15 @@ void waitpidTest() {
     printf(1, "Testing waitpid\n");
 
     int pid = fork();
-    int exitvar;
-    int parentpid;
+    int exitvar = 0;
+    int parentpid = 2;
 
     if (pid == 0) {
         printf(1, "This is the child with pid %d\n", pid);
         printf(1, "It will always print the first 3 letters a-c: ");
         printf(1, "a, b, c\n");
         printf(1, "The child with pid: %d exits with status %d\n", pid, exitvar);
+        exit(0);
     }
     else {
         parentpid = waitpid(pid, &exitvar, 0);
@@ -81,6 +82,7 @@ void waitpidTest() {
         printf(1, "It will always print the last 3 letters x-z: ");
         printf(1, "x, y, z\n");
         printf(1, "The parent with pid: %d exits with status %d\n", parentpid, exitvar);
+        exit(0);
 
     }
 
